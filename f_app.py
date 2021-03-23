aeced32b04d7159a2ed3a909c6ca15110ef52d02
Memhir-Yasue <eyasu.woldu@temple.edu>
2021-03-23 19:10:52 -0400

    working

--- f_app.py
+++ f_app.py
@@ -1,20 +1,41 @@
 import os
 
 from flask import Flask, flash, redirect,  render_template, request, url_for, send_from_directory
-from segmentation.seg import run_segmenter
+from segmentation.seg import run_segmenter, save_plt
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'static/input'
+OUTPUT_FOLDER = 'static/output'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 
 @app.route('/upload/<filename>')
 def uploaded_file(filename):
+    
     print(app.config['UPLOAD_FOLDER'])
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/processed/<filename>')
+def processed_image(filename):
+    f_path = app.config['OUTPUT_FOLDER']+"/"+filename
+    print(f_path)
+    return send_from_directory(app.config['OUTPUT_FOLDER'], filename)
+
+
+@app.route('/process/<filename>', methods=['GET'])
+def process_image(filename):
+    
+    img = run_segmenter(img_path='static/input/'+filename)
+    extension = filename.rsplit('.', 1)[1].lower()
+    output_f_name = "output."+extension
+    save_plt(img, output_f_name)
+
+    return redirect(url_for('processed_image', filename=output_f_name))
 
 
 @app.route('/')
@@ -39,16 +60,14 @@ def upload():
             filename = secure_filename(file.filename)
             extension = filename.rsplit('.', 1)[1].lower()
             neo_file_name =  f"input.{extension}"
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], neo_file_name))
-            return redirect(url_for('uploaded_file', filename=neo_file_name))
+            f_path = os.path.join(app.config['UPLOAD_FOLDER'], neo_file_name)
+            file.save(f_path)
+            return redirect(url_for('process_image', filename=neo_file_name))
+            # return redirect(url_for('uploaded_file', filename=neo_file_name))
         else:
             return render_template('upload_failed.html')
 
     return render_template('upload.html')
-
-# @app.route('/process', methods=['GET', 'POST'])
-# def process(filePath):
-
 
 
 # @app.route('/uploads/<filename>')
